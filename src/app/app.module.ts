8d1beeaf00440919cacb4fde49afc9f58af30615
Jason <jason@lymannuthouse.com>
2019-09-01 16:29:45 -0700

    Components & styling
    Added individual page components. Added styling to homepage.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { AboutComponent } from './about/about.component';
+import { WorkHistoryComponent } from './work-history/work-history.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    AboutComponent,
+    WorkHistoryComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'skills', component: SkillsComponent },
+      { path: 'work-history', component: WorkHistoryComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
