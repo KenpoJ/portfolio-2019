2999f83c53fc714c85c8e21c3503b998c435c92f
Jason <jason@lymannuthouse.com>
2019-09-08 12:39:28 -0700

    Update Skills page
    Updated list of skills and added JS behavior for tabbed nav

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
 
-var n = $('.nav');
-var l = $('.nav a');
+var n = $('.tab-container .nav');
+var l = $('.tab-container .nav a');
+
+$('.tab-pane').hide();
+$('#core').show();
 
 l.click(function(e) {
   e.preventDefault();
+  var href = $(this).attr('href');
+
+  l.removeClass('active');
+  $(this).addClass('active');
+  
+  $('.tab-pane').fadeOut('slow', function() {
+    $(href).fadeIn('slow');
+  });
 })
 
 })
