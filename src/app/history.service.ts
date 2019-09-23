c0f4850477aca9268487414dfe2c2beadeac8ccb
Jason <jason@lymannuthouse.com>
2019-09-22 21:22:05 -0700

    Changes to Styles, Ready for Work History Service

---
+++ src/app/history.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Job } from './Job';
+// import { Jobs } from '../assets/jobs.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistoryService {
+
+  constructor() { }
+}
--- src/app/work-history/work-history.component.ts
+++ src/app/work-history/work-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HistoryService } from '../history.service';
 
 @Component({
   selector: 'app-work-history',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkHistoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  ) {}
 
   ngOnInit() {
   }
